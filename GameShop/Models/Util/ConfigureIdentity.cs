dda7530f4ba7b65dab81813506416262da6c5c16
James Vernon <Vernon.James@SPCollege.edu>
2024-02-26 10:07:45 -0500

    update Identitiy

--- GameShop/Models/Util/ConfigureIdentity.cs
+++ GameShop/Models/Util/ConfigureIdentity.cs
@@ -7,7 +7,11 @@ namespace GameShop.Models
         
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         public static async Task CreateAdminUserAsync(IServiceProvider serviceProvider)
+=======
+        public static async Task CreateAdminUserAsync(IServiceProvider serviceProvider, )
+>>>>>>> c0b109c156901a71128da7efbb856131a2a4a2e2
 =======
         public static async Task CreateAdminUserAsync(IServiceProvider serviceProvider, )
 >>>>>>> c0b109c156901a71128da7efbb856131a2a4a2e2
