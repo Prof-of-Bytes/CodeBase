bdd9058ecfc1e7b6628829d3901d0576e97d8d7a
James Vernon <Vernon.James@SPCollege.edu>
2024-03-29 14:10:30 -0400

    push to main remote

--- GameShop/GameShop/obj/Debug/net8.0/GameShop.AssemblyInfo.cs
+++ GameShop/GameShop/obj/Debug/net8.0/GameShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3205f2fb18728c68e0faea540f74ca32c104d300")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
